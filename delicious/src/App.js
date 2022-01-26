c1310a1ac3db715af4e0485ab40e62920acf0c49
Sadiksha <sadiksha.chitrakar101@gmail.com>
2022-01-26 17:06:23 +0545

    Headers & MealItem

--- delicious/src/App.js
+++ delicious/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
-    <div className="App">
-      <p>Let's get started</p>
-    </div>
+    <>
+      <Header/>
+      <main>
+        <Meals/>
+      </main>
+    </>
   );
 }
 
---
+++ delicious/src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+
+import React from 'react';
+
+const Meals = () => {
+  return <>
+  <MealsSummary/>
+  <AvailableMeals/>
+  </>;
+};
+
+export default Meals;
