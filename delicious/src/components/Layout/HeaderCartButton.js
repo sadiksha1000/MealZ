b40df2a3d7b599bdda3c4e839058aef5e5d8b8b0
Sadiksha <sadiksha.chitrakar101@gmail.com>
2022-01-28 18:55:53 +0545

    idunnowhat'shappening

--- delicious/src/components/Layout/HeaderCartButton.js
+++ delicious/src/components/Layout/HeaderCartButton.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+  const cartCtx=useContext(CartContext);
+  const numberOfCartItems=cartCtx.items.reduce((currentNumber,item)=>{
+    return currentNumber+item.amount;
+  },0);
+
   return <button className={classes.button} onClick={props.onClicked}>
       <span className={classes.icon}>
         <CartIcon />
@@ -11,7 +17,7 @@ const HeaderCartButton = (props) => {
           Your Cart
       </span>
       <span className={classes.badge}>
-          3
+          {numberOfCartItems}
       </span>
   </button>;
 };
--- delicious/src/store/CartProvider.js
+++ delicious/src/store/CartProvider.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React,{useReducer} from 'react';
 import CartContext from './cart-context';
 
-const CartProvider=(props)=>{
+const defaultCartState={
+    items:[],
+    totalAmount:0
+}
 
+const cartReducer=(state,action)=>{
+    return defaultCartState;
+};
+
+// manage the cart context data and provide to those components that are listening to this component
+const CartProvider=(props)=>{
+    const [cartState,dispatchCartAction]=useReducer(cartReducer,defaultCartState);
     const addItemToCartHandler=(item)=>{};
     const removeItemFromCartHandler=(id)=>{};
     const cartContext={
